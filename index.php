5be0ad29eb4e48a3201c921009c038b45e5c09b5
Flávia Gabriella <flaviagabriella.cs@gmail.com>
2025-05-21 15:03:33 -0300

    Mudança de pasta do index, adição de três novas páginas na pasta, adição do arquivo htacces e correção de alguns erros

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>RH Móveis Planejados</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
+
+  <link rel="shortcut icon" href="imagens/icone.png">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body class="didact-gothic-regular">
+
+  <header>
+    <nav id="navbar" class="fixed-top">
+      <ul class="nav nav-underline justify-content-center">
+        <li>
+          <a href="home" class="logo ">
+            <img src="imagens/logo.png" alt="Logo da empresa RH Móveis Planejados">
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" title="Home" href="home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" title="Produtos" href="#produtos">Produtos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" title="Sobre" href="#sobre">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" title="Contato" href="contato">Contato</a>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
+  <main>
+    <?php
+
+      $pages = "home";
+      if(isset($_GET["param"])){
+        $param = explode("/", $_GET["param"]);
+        $pages = $param[0];
+      }
+
+      $pages = "pages/{$pages}.php";
+
+      if(file_exists($pages)){
+        include $pages;
+      }else{
+        include "pages/erro.php";
+      }
+
+    ?>
+
+  </main>
+
+  <footer>
+    <div class="footer">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="conteudo-footer">
+              <p>Telefone: (44) 99860-8027</p>
+              <p>Endereço: Rua</p>
+              <p>Todos os direitos reservados</p>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="links">
+              <a class="icon" href="https://www.instagram.com/_rh_moveis" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+              <a class="icon" href="https://wa.me/5544998608027" target="_blank">
+                <i class="fa-brands fa-whatsapp"></i>
+              </a>
+              <a class="icon" href="https://www.facebook.com/rhmoveisplanejado" target="_blank">
+                <i class="fa-brands fa-facebook"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+
+  <script src="js/navbar.js"></script>
+  <script src="js/carrossel.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+    crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+  <script src="https://kit.fontawesome.com/2e7313998e.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- pages/home.php
+++ pages/home.php
@@ -1,59 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>RH Móveis Planejados</title>
-
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
-
-  <link rel="shortcut icon" href="../imagens/icone.png">
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-  <link rel="stylesheet" href="../css/style.css">
-
-</head>
-
-<body class="didact-gothic-regular">
-  <header>
-
-    <nav id="navbar" class="fixed-top">
-      <ul class="nav nav-underline justify-content-center">
-        <li>
-          <a href="#" class="logo ">
-            <img src="../imagens/logo.png" alt="Logo da empresa RH Móveis Planejados">
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" title="Home" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" title="Produtos" href="#produtos">Produtos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" title="Sobre" href="#sobre">Sobre</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" title="Contato" href="#">Contato</a>
-        </li>
-      </ul>
-    </nav>
-
-  </header>
-  <main>
     <div class="banner">
       <div class="conteudo-banner">
         <h1>Transforme seu espaço com móveis planejados sobmedida!</h1>
         <a href="#produtos" class="btn btn-outline-light">Ver produtos</a>
       </div>
       <div class="container-imagem">
-        <img class="imagem-banner cima img-fluid" src="../imagens/imagembanner.jpg"
+        <img class="imagem-banner cima img-fluid" src="imagens/imagembanner.jpg"
           alt="Uma foto de uma cozinha que foi um dos trabalhos da empresa">
-        <img class="imagem-banner baixo img-fluid" src="../imagens/color.jpg"
+        <img class="imagem-banner baixo img-fluid" src="imagens/color.jpg"
           alt="Cor amarela servindo como fundo para a foto">
       </div>
     </div>
@@ -71,13 +24,13 @@
               <div class="carousel-item active">
                 <div class="row">
                   <div class="col-4">
-                    <img src="../imagens/cozinha-verde.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Verde">
+                    <img src="imagens/cozinha-verde.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Verde">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Preta">
+                    <img src="imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Preta">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom">
                   </div>
                 </div>
@@ -85,15 +38,15 @@
               <div class="carousel-item">
                 <div class="row">
                   <div class="col-4">
-                    <img src="../imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Preta 2">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom 2">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom 2">
 
                   </div>
@@ -126,13 +79,13 @@
               <div class="carousel-item active">
                 <div class="row">
                   <div class="col-4">
-                    <img src="../imagens/cozinha-verde.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Verde">
+                    <img src="imagens/cozinha-verde.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Verde">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Preta">
+                    <img src="imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Preta">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom">
                   </div>
                 </div>
@@ -140,15 +93,15 @@
               <div class="carousel-item">
                 <div class="row">
                   <div class="col-4">
-                    <img src="../imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Preta 2">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom 2">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom 2">
 
                   </div>
@@ -181,13 +134,13 @@
               <div class="carousel-item active">
                 <div class="row">
                   <div class="col-4">
-                    <img src="../imagens/cozinha-verde.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Verde">
+                    <img src="imagens/cozinha-verde.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Verde">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Preta">
+                    <img src="imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel" alt="Cozinha Preta">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom">
                   </div>
                 </div>
@@ -195,17 +148,16 @@
               <div class="carousel-item">
                 <div class="row">
                   <div class="col-4">
-                    <img src="../imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-preto.jpg" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Preta 2">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom 2">
                   </div>
                   <div class="col-4">
-                    <img src="../imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
+                    <img src="imagens/cozinha-marrom.webp" class="d-block w-100 imagem-carrossel"
                       alt="Cozinha Marrom 2">
-
                   </div>
                 </div>
               </div>
@@ -230,7 +182,7 @@
 
     <div id="sobre">
       <div class="quemsomos">
-        <img class="imagem-banner-sobre" src="../imagens/bannerrh.jpg" alt="">
+        <img class="imagem-banner-sobre" src="imagens/bannerrh.jpg" alt="">
         <div class="conteudo-sobre">
           <div class="texto-sobre">
             <h3>QUEM SOMOS</h3>
@@ -239,57 +191,8 @@
               doloremque asperiores consectetur nisi?</p>
           </div>
           <div>
-            <img class="baixo-sobre" src="../imagens/colorblue.jpg" alt="">
+            <img class="baixo-sobre" src="imagens/colorblue.jpg" alt="">
           </div>
         </div>
       </div>
     </div>
-
-  </main>
-
-  <footer>
-    <div class="footer">
-      <div class="container text-center">
-        <div class="row">
-          <div class="col">
-            <div class="conteudo-footer">
-              <p>Telefone: (44) 12345-1234</p>
-              <p>Endereço: Rua</p>
-              <p>Todos os direitos reservados</p>
-            </div>
-          </div>
-
-          <div class="col">
-            <div class="links">
-              <a class="icon" href="https://www.instagram.com/_rh_moveis" target="_blank">
-                <i class="fa-brands fa-instagram"></i>
-              </a>
-              <a class="icon" href="" target="_blank">
-                <i class="fa-brands fa-whatsapp"></i>
-              </a>
-              <a class="icon" href="https://www.facebook.com/rhmoveisplanejado" target="_blank">
-                <i class="fa-brands fa-facebook"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-
-
-
-  <script src="../js/navbar.js"></script>
-  <script src="../js/carrossel.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
-    crossorigin="anonymous"></script>
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script>
-    AOS.init();
-  </script>
-  <script src="https://kit.fontawesome.com/2e7313998e.js" crossorigin="anonymous"></script>
-</body>
-
-</html>

7e0639d0fe3076c8b508c7438d7d59cee1c69699
Flávia Gabriella <flaviagabriella.cs@gmail.com>
2025-05-21 22:02:18 -0300

    Adição de novas páginas e estilização de algumas partes

---
+++ pages/contato.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="css/contato.css">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+ 
+<h2 class="text-center">Entre em contato!</h2>
+
+<form action="" method="POST" id="contact-form">
+    <label for="name">Nome</label>
+    <input type="text" name="name" class="name" id="name" autocomplete="off" required>
+
+    <label for="email">E-mail</label>
+    <input type="email" name="email" class="email" id="email" autocomplete="off" required>
+
+    <label for="message">Sua mensagem</label>
+    <textarea type="text" name="message" class="message" id="message" autocomplete="off" required></textarea>
+
+    <button type="submit">Enviar</button>
+</form>
+
+<div class="col">
+    <div class="links">
+        <a class="icon" href="https://www.instagram.com/_rh_moveis" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a class="icon" href="https://wa.me/5544998608027" target="_blank">
+            <i class="fa-brands fa-whatsapp"></i>
+        </a>
+        <a class="icon" href="https://www.facebook.com/rhmoveisplanejado" target="_blank">
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+    </div>
+</div>
---
+++ pages/erro.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="css/erro.css">
+<h2 class="text-center">Página não encontrada!</h2>
+<p class="text-center">
+    A página que você está tentando acessar não existe ou foi removida.
+</p>
